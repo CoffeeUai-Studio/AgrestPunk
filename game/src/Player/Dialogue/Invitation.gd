978c20eb4428920b158b0c5e1b8a842162d8362e
CoffeeUai-Nick <CoffeeUai-Studio@users.noreply.github.com>
2019-10-29 01:01:07 -0300

    # 07 - Parseiro Jasons
    -Lê e faz parse de arquivos .json

--- game/src/Player/Dialogue/Invitation.gd
+++
@@ -1,8 +0,0 @@
-
-extends "res://Scripts/Item.gd"
-
-func _ready():
-	pass
-
-func update_choices(choices):
-	choices["haveInvitation"] = true
--- game/src/UI/Menu/Title.gd
+++ game/src/UI/Menu/Title.gd
@@ -1,8 +1,26 @@
 extends Control
 
+export (String, FILE) var text_dialogue
+#var result_json = JSON.parse(text_json)
+#var result = {}
 
 func _ready():
 	$Menu/CenterRow/Buttons/PlayButton.grab_focus()
+	var data_file = File.new()
+	if data_file.open(text_dialogue, File.READ) != OK:
+		return
+	var data_text = data_file.get_as_text()
+	data_file.close()
+	var data_parse = JSON.parse(data_text)
+	if data_parse.error != OK:
+		return
+		#print("Error: ", result_json.error)
+		#print("Error Line: ", result_json.error_line)
+		#print("Error String: ", result_json.error_string)
+	var data = data_parse.result
+	print(data["001"]["text"])
+	print(data["002"]["text"])
+	
 
 
 func _process(delta):
